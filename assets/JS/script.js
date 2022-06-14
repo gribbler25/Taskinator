dd85fa17d8638e21a7f74648ee0876f81fb0a989
gribbler25 <98048059+gribbler25@users.noreply.github.com>
2022-06-14 17:30:19 -0500

    task type and info working

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -7,9 +7,22 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function (event) {
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name= 'task-name']").value;
+    var taskTypeInput = document.querySelector("select[name= 'task-type']").value;
+
+    //create list item
     var listItemEl = document.createElement("li");
-    listItemEl.textContent = "this is a new task!";
     listItemEl.className = "task-item";
+
+    //create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    //add html content to the div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span >"
+    // listItemEl.textContent = taskNameInput;
+    listItemEl.appendChild(taskInfoEl);
+
     tasksToDoEl.appendChild(listItemEl);
 
 };
