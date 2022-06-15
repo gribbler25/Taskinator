5732271d06c5bae925f9d8ce24dbfdb34e842c9e
gribbler25 <98048059+gribbler25@users.noreply.github.com>
2022-06-14 19:21:04 -0500

    form handling complete with error handling

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -10,6 +10,11 @@ var taskFormHandler = function (event) {
     var taskNameInput = document.querySelector("input[name= 'task-name']").value;
     var taskTypeInput = document.querySelector("select[name= 'task-type']").value;
 
+    if (!taskNameInput || !taskTypeInput) {
+        alert("First first fill out the task form!");
+        return false;
+    }
+    formEl.reset();
     var taskDataObj = {
         name: taskNameInput,
 
